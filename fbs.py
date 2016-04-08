c60eee6ec8ecd1e88f5a29be333fbe74e9994e05
云胡不喜 <weimingjie@Live.com>
2016-04-08 19:44:38 +0800

    this is my second python program

---
+++ fbs.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import random,time,queue
+from multiprogressing.managers import BaseManager
+
+task_queue = queue.Queue();
+
+result_queue = queue.Queue();
+
+class QueueManager(BaseManager):
+    pass
