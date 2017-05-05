2f4a5a8092c905a8004bbec66151137bb1eb532e
tofo <weimingjie@live.com>
2017-05-05 20:59:28 +0800

    lesson2

---
+++ lesson2.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+from urllib import request
+from urllib import error
+
+if __name__ == "__main__":
+    # 一个不存在的连接
+    url = "http://www.douyu.com/Jack_Cui.html"
+    req = request.Request(url)
+    try:
+        responese = request.urlopen(req)
+        #html = response.read().decode('utf-8')
+        # print(html)
+    except error.URLError as e:
+        if hasattr(e, "code"):
+            print(e.code)
+        elif hasattr(e, "reason"):
+            print(e.reason)
