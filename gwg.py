516a0735bd47ad77d6ccd8e4b23e7040fe977ec9
哇弥陀佛 <weimingjie@Live.com>
2017-05-22 14:21:13 +0800

    简化gwg查询

--- gwg.py
+++ gwg.py
@@ -3,13 +3,9 @@ from urllib import request
 from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
-    # 以CSDN为例，CSDN不更改User Agent是无法访问的
     url = 'https://petitions.whitehouse.gov/'
-    head = {}
-    # 写入User Agent信息
-    head['User-Agent'] = 'test'
- # 创建Request对象
-    req = request.Request(url, headers=head)
+    # 创建Request对象
+    req = request.Request(url)
     # 传入创建好的Request对象
     response = request.urlopen(req)
     # 读取响应信息并解码
@@ -17,9 +13,5 @@ if __name__ == "__main__":
     # 打印信息
     # 创建Beautiful Soup对象
     soup = BeautifulSoup(html, 'lxml')
-    for i in soup.find_all(attrs={"data-nid": "2545476"}):
-        one = i.find_all("span", attrs={"class": "signatures-number"})
-        print(one[0].string)
-
-
-#=""
+    tag = soup.find(attrs={"data-nid": "2545476"})
+    print(tag.find("span", attrs={"class": "signatures-number"}).string)
