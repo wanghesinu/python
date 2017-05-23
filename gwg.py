236bda9bcd2b4ac49ee86fb13bda1f9bf892b13b
哇弥陀佛 <weimingjie@Live.com>
2017-05-23 16:52:30 +0800

    格式化信息

--- gwg.py
+++ gwg.py
@@ -1,17 +1,24 @@
 # -*- coding: UTF-8 -*-
+import datetime
 from urllib import request
 from bs4 import BeautifulSoup
 
+
 if __name__ == "__main__":
     url = 'https://petitions.whitehouse.gov/'
-    # 创建Request对象
     req = request.Request(url)
-    # 传入创建好的Request对象
     response = request.urlopen(req)
-    # 读取响应信息并解码
     html = response.read().decode('utf-8')
-    # 打印信息
     # 创建Beautiful Soup对象
     soup = BeautifulSoup(html, 'lxml')
     tag = soup.find(attrs={"data-nid": "2545476"})
-    print(tag.find("span", attrs={"class": "signatures-number"}).string)
+    count = tag.find("span", attrs={"class": "signatures-number"}).string
+    now = datetime.datetime.now()
+    dead_line = datetime.datetime.strptime(
+        '2017-06-18', '%Y-%m-%d')
+    delta = dead_line - now
+    per = (30 - delta.days) / 30 * 100
+    print("  all_count : %s" % count)
+    print("  left_days : %d" % (30 - delta.days))
+    print("remain_days : %d" % delta.days)
+    print("    percent : %.2f%%" % per)
